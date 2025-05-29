eb1e44011e4d599265ec46fd9950aca9dd7eb74e
zikohad24 <zikohaddad123@gmail.com>
2025-05-29 19:26:14 +0200

    Update generate_docs.py

--- generate_docs.py
+++ generate_docs.py
@@ -1,10 +1,19 @@
 # generate_docs.py
+
 import os
 import django
 import pydoc
 
+# Configura les variables d'entorn per a Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_site.settings")
 django.setup()
 
-pydoc.writedoc("blog.models")
-pydoc.writedoc("blog.urls")
+# Llista de mòduls que vols documentar
+modules_to_doc = [
+    "blog.models",
+    "blog.urls",
+]
+
+# Genera els fitxers HTML a la carpeta actual
+for module in modules_to_doc:
+    pydoc.writedoc(module)
